a68ab42c45b6e8cd4afa854abf1e7c1c564103e2
Ajaya Kumar Behera <ajaya.physics@gmail.com>
2024-12-28 17:05:05 +0900

    Code: python first_script.py

---
+++ python first_script.py
@@ -0,0 +1,23 @@
+Hello, World!
+The result of 5 + 3 is: 8
+Congratulations on running your first Python script!
+print("My name is [Your Name]")
+product = 7 * 6
+print("The product of 7 and 6 is:", product)
+
+---
+
+### `first_script.py` Code
+
+```python
+# This is my first Python script
+
+# Print a simple message
+print("Hello, World!")
+
+# Perform a basic calculation
+result = 5 + 3
+print("The result of 5 + 3 is:", result)
+
+# Print a motivational message
+print("Congratulations on running your first Python script!")
